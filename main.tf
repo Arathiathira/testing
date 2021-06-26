50686b7fabd441fbb6aa4c0455ade761df2234a9
Arathiathira <82867841+Arathiathira@users.noreply.github.com>
2021-06-26 12:53:22 +0530

    Update main.tf
    added access keys

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ terraform {
      storage_account_name = "s1234567abc"
      container_name       = "c1234567abc"
      key                  = "terraform.tfstate"
+     access_keys          ="wQeOkNzkMygUusQIdiRD1xNo1Vtl8GzZ2pc3VpFdZLNv+1CXSORrjMbIuEbVP/awZuo1+4W17sXEeMkDMyhQRQ=="
         features {}
     }
 }
