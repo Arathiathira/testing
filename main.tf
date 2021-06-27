947c5523c54539098205af9e0a7ebc8b6098bdcc
Arathiathira <82867841+Arathiathira@users.noreply.github.com>
2021-06-27 13:01:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
     required_version = ">= 0.11"
     backend "azurerm"{
-     resource_group_name  =  "arsrg"
-     storage_account_name = "s1234567abcd"
-     container_name       = "c1234567abcd"
+     resource_group_name  =  "r1234567abc"
+     storage_account_name = "s1234567abc"
+     container_name       = "c1234567abc"
      key                  = "terraform.tfstate"
      access_keys          ="wQeOkNzkMygUusQIdiRD1xNo1Vtl8GzZ2pc3VpFdZLNv+1CXSORrjMbIuEbVP/awZuo1+4W17sXEeMkDMyhQRQ=="
      connection_string    ="DefaultEndpointsProtocol=https;AccountName=s1234567abc;AccountKey=wQeOkNzkMygUusQIdiRD1xNo1Vtl8GzZ2pc3VpFdZLNv+1CXSORrjMbIuEbVP/awZuo1+4W17sXEeMkDMyhQRQ==;EndpointSuffix=core.windows.net"
